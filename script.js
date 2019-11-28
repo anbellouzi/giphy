f2964a8f6ff0c33f8e433bb662f04acfd92ae29a
Anas Bellouzi <anas.bellouzi@students.makeschool.com>
2019-11-27 19:37:51 -0800

    click gif for large display

--- script.js
+++ script.js
@@ -3,16 +3,36 @@
 // giphy API key
 const apiKey = 'BLYcLJA9Uc2SlY1iloQhooj9P85PTqzs'
 
+const container = document.getElementById('container')
 const resultsEl = document.getElementById('results')
 const searchForm = document.getElementById('searchForm')
 const searchInput = document.getElementById('searchInput')
+const gifItem = document.getElementById('gif_item')
+const show_gif_box = document.getElementById('show_gif')
+const show_gif_image = document.getElementById('show_image')
 
 // search button clicked
 searchForm.addEventListener('submit', function(e) {
   e.preventDefault()
   const q = searchInput.value
   search(q)
-})
+});
+
+// gif item clicked
+function showGif(id) {
+  show_gif_image.src = id
+  show_gif_box.classList.remove("display_off");
+  show_gif_box.classList.add("display_on");
+  container.classList.add("fade");
+}
+
+// x close icon
+function closeGif() {
+  show_gif_image.src = ""
+  container.classList.remove("fade");
+  show_gif_box.classList.remove("display_on");
+  show_gif_box.classList.add("display_off");
+}
 
 // Begining search function
 function search(q) {
@@ -23,11 +43,20 @@ function search(q) {
     let resultsHTML = ""
 
     json.data.forEach(function(obj) {
+      const largeImg = obj.images.downsized_still.url
+      console.log(largeImg)
       const url = obj.images.fixed_width.url
       const width = obj.images.fixed_width.width
       const height = obj.images.fixed_width.height
       const title = obj.title
-      resultsHTML += `<img class="item" src="${url}" "width=${width}" height="${height}" alt="${title}">`
+      resultsHTML += `<a href="#" id="${largeImg}" onclick="showGif(this.id)">
+                        <img
+                        class="item"
+                        src="${url}"
+                        width="${width}"
+                        height="${height}"
+                        alt="${title}">
+                      </a>`
     })
 
     resultsEl.innerHTML = resultsHTML
@@ -36,5 +65,7 @@ function search(q) {
     console.log(err.message)
   })
 }
+
+
 // End search function
 search("cars")
