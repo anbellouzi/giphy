2bc4ad56715ebd380dc6ef812f4fdffb0985ae49
Anas Bellouzi <anas.bellouzi@students.makeschool.com>
2019-11-28 00:24:36 -0800

    added pagination and image size option

--- script.js
+++ script.js
@@ -7,18 +7,61 @@ const container = document.getElementById('container')
 const resultsEl = document.getElementById('results')
 const searchForm = document.getElementById('searchForm')
 const searchInput = document.getElementById('searchInput')
+const searchSize = document.getElementById('searchSize')
 const gifItem = document.getElementById('gif_item')
 const show_gif_box = document.getElementById('show_gif')
 const show_gif_image = document.getElementById('show_image')
+const next_button = document.getElementById('button_next')
+const back_button = document.getElementById('button_back')
+
+var queryTerm = "Trending"
+var imageSize = "medium"
+var page = 0
+var pageIndex = 10
+var gifLimit = 10
+
+// next page button clicked
+next_button.addEventListener('click', function(e) {
+  e.preventDefault()
+  page += pageIndex
+  search(queryTerm)
+})
+
+// back page button clicked
+back_button.addEventListener('click', function(e) {
+  e.preventDefault()
+  if (page <= pageIndex) {
+      page = 0
+  }
+  else {
+    page -= pageIndex
+  }
+  search(queryTerm)
+})
 
 // search button clicked
-searchForm.addEventListener('submit', function(e) {
+searchForm.addEventListener('change', function(e) {
   e.preventDefault()
-  const q = searchInput.value
-  search(q)
+  queryTerm = searchInput.value
+  imageSize = searchSize.value
+  if(queryTerm != "") {
+      search(queryTerm, imageSize, pageIndex)
+  }
+  else {
+
+      search(queryTerm, imageSize, pageIndex)
+  }
 });
 
-// gif item clicked
+// size changed
+searchSize.addEventListener('change', function(e) {
+  // e.preventDefault()
+  imageSize = searchSize.value
+  changeToSize(imageSize)
+
+});
+
+// gif item onclick
 function showGif(id) {
   const caption = document.getElementById('caption')
   caption.innerHTML = document.getElementById(id).text
@@ -38,7 +81,7 @@ function closeGif() {
 
 // Begining search function
 function search(q) {
-  const path = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${q}`
+  const path = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${q}&limit=${gifLimit}&offset=${page}`
   fetch(path).then(function(res) {
     return res.json()
   }).then(function(json) {
@@ -57,24 +100,72 @@ function search(q) {
 // create a gif html here
 function createGif(obj) {
   let resultsHTML = ""
+  var url = null
+  var width = null
+  var height = null
+
   const largeImg = obj.images.downsized_still.url
-  console.log(largeImg)
-  const url = obj.images.fixed_width.url
-  const width = obj.images.fixed_width.width
-  const height = obj.images.fixed_width.height
+  // console.log(obj)
+
+  if (imageSize == "small") {
+    url = obj.images.fixed_height_small.url
+    width = obj.images.fixed_height_small.width
+    height = obj.images.fixed_height_small.height
+  }
+  else if(imageSize == "medium") {
+    url = obj.images.fixed_width.url
+    width = obj.images.fixed_width.width
+    height = obj.images.fixed_width.height
+  }
+  else {
+    url = obj.images.fixed_height_still.url
+    width = obj.images.fixed_height_still.width
+    height = obj.images.fixed_height_still.height
+  }
   const title = obj.title
-  resultsHTML += `<a class="gif_anchor" href="#" id="${largeImg}" onclick="showGif(this.id)">
+  resultsHTML += `<a class="gif_anchor w3-animate-zoom" href="#" id="${largeImg}" onclick="showGif(this.id)">
                     <img
-                    class="item"
+                    class="item w3-animate-zoom"
                     src="${url}"
-                    width="${width}"
-                    height="${height}"
+                    width="100%"
+                    height="auto"
                     alt="${title}">
                     ${title}
                   </a>`
 
+  changeToSize(imageSize)
   return resultsHTML
 }
 
+function changeToSize(size) {
+  if (size == 'small') {
+    container.classList.remove('medium')
+    container.classList.remove('large')
+    resultsEl.classList.remove('column-small')
+    resultsEl.classList.remove('column-large')
+
+    container.classList.add('small')
+    resultsEl.classList.add('column-small')
+  }
+  else if (size == 'medium') {
+    container.classList.remove('large')
+    container.classList.remove('small')
+    resultsEl.classList.remove('column-small')
+    resultsEl.classList.remove('column-large')
+
+    container.classList.add('medium')
+    resultsEl.classList.add('column-medium')
+  }
+  else {
+    container.classList.remove('medium')
+    container.classList.remove('small')
+    resultsEl.classList.remove('column-small')
+    resultsEl.classList.remove('column-medium')
+
+    container.classList.add('large')
+    resultsEl.classList.add('column-large')
+  }
+}
+
 // End search function
-search("Trending")
+search("Trending", "large")
